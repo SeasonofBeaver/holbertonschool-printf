d7ce0e7ec4d429e255fb3a2deb37a0b41812aa36
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-07-22 15:06:12 +0200

    push to main file

--- main.h
+++ main.h
@@ -8,5 +8,15 @@
 #include <string.h>
 
 int _printf(const char *format, ...);
+int itos(int num, char *str);
+void reverse(char *str, int len);
+
+struct formats
+{
+	char *type;
+	void (*function)(va_list);
+};
+
+typedef struct formats search;
 
 #endif /* main_h */
