3c2839abad8b03d24bedc441f0875832001292b7
MrKay12 <quentin.lepoutre12@gmail.com>
2024-07-23 10:47:35 +0200

    clean main.h

--- main.h
+++ main.h
@@ -8,19 +8,13 @@
 #include <string.h>
 
 int _printf(const char *format, ...);
-<<<<<<< HEAD
+
 unsigned int itos(unsigned int num, char *str);
 int itosn(int num, char *str);
 void reverse(char *str, int len);
 int uitoso(unsigned int num, char *str);
 int uitosh(unsigned int num, char *str, int uppercase);
-
-=======
-int itos(int num, char *str);
-void reverse(char *str, int len);
-int uitosh(unsigned int num, char *str, int uppercase);
 char *convert(unsigned long int num, int base, int lowercase);
->>>>>>> test-Jakob
 
 struct formats
 {
